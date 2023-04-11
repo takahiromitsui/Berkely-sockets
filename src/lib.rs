158c1ae09e2385528b4e7f37bec6582fcb9c945b
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2023-04-11 11:37:19 +0200

    feat: Create functions to start server

---
+++ src/lib.rs
@@ -0,0 +1,51 @@
+pub fn stream_socket() -> i32 {
+    // AF_INET = IPv4
+    let domain = nix::sys::socket::AddressFamily::Inet;
+    // SOCK_STREAM = TCP
+    let socket_type = nix::sys::socket::SockType::Stream;
+    // Protocol = TCP
+    let protocol = nix::sys::socket::SockProtocol::Tcp;
+
+    // Additional flags
+    let flags = nix::sys::socket::SockFlag::empty();
+
+    let fd = nix::sys::socket::socket(domain, socket_type, flags, protocol).unwrap();
+    println!("Created socket with fd: {}", fd);
+    fd
+}
+
+pub fn bind(sockfd: i32, my_addr: &nix::sys::socket::SockAddr) -> i32 {
+    let res = nix::sys::socket::bind(sockfd, my_addr);
+    match res {
+        Ok(_) => {
+            println!("Bind successful");
+            0
+        }
+        Err(e) => {
+            println!("Bind failed: {}", e);
+            -1
+        }
+    }
+}
+
+pub fn listen(sockfd: i32, backlog: usize) -> i32 {
+    let res = nix::sys::socket::listen(sockfd, backlog);
+    match res {
+        Ok(_) => {
+            println!("Listen successful");
+            0
+        }
+        Err(e) => {
+            println!("Listen failed: {}", e);
+            -1
+        }
+    }
+}
+
+pub fn start(my_addr: &nix::sys::socket::SockAddr) {
+    let sockfd = stream_socket();
+    bind(sockfd, &my_addr);
+    println!("[LISTENING] Listening on port: {}", my_addr);
+    listen(sockfd, 10);
+    println!("[STARTING] Server started");
+}
---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use network_programming::start;
+
+fn main() {
+    let my_addr = nix::sys::socket::SockAddr::new_inet(nix::sys::socket::InetAddr::new(
+        nix::sys::socket::IpAddr::V4(nix::sys::socket::Ipv4Addr::new(127, 0, 0, 1)),
+        8080,
+    ));
+    start(&my_addr)
+}
