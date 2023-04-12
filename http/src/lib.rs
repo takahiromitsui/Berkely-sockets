54a86d71c22fe58878d7f4a0989cb4bef7c7527d
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2023-04-12 13:44:27 +0200

    feat: Create parse_http_request

--- http/src/lib.rs
+++ http/src/lib.rs
@@ -1,11 +1,57 @@
 use std::{
+    io::Read,
     net::TcpStream,
     sync::{mpsc, Arc, Mutex},
     thread,
 };
 
-pub fn handle_connection(stream: TcpStream) {
-    println!("Connection established");
+pub fn parse_http_request(buffer: &[u8]) -> Option<(String, Vec<(String, String)>, String)> {
+    let mut headers_start = None;
+    let mut headers_end = None;
+    let mut body_start = None;
+    for (i, chunk) in buffer.windows(2).enumerate() {
+        if chunk == b"\r\n" {
+            if headers_start.is_none() {
+                headers_start = Some(i + 2);
+            } else if headers_end.is_none() {
+                headers_end = Some(i);
+                body_start = Some(i + 2);
+                break;
+            }
+        }
+    }
+    if let (Some(start), Some(end), Some(body_start)) = (headers_start, headers_end, body_start) {
+        let request_line = String::from_utf8_lossy(&buffer[..start - 2]).into_owned();
+        let headers = String::from_utf8_lossy(&buffer[start..end]).into_owned();
+        let body = String::from_utf8_lossy(&buffer[body_start..]).into_owned();
+        let headers = headers
+            .split("\r\n")
+            .filter(|line| !line.is_empty())
+            .map(|line| {
+                let mut parts = line.splitn(2, ": ");
+                (
+                    parts.next().unwrap().to_owned(),
+                    parts.next().unwrap().to_owned(),
+                )
+            })
+            .collect();
+        Some((request_line, headers, body))
+    } else {
+        None
+    }
+}
+
+pub fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    let req = match parse_http_request(&buffer) {
+        Some(req) => req,
+        None => return,
+    };
+    let (request_line, headers, body) = req;
+    println!("Request line: {}", request_line);
+    println!("Headers: {:?}", headers);
+    println!("Body: {}", body);
 }
 
 pub struct ThreadPool {
@@ -64,7 +110,7 @@ impl Worker {
             let message = receiver.lock().unwrap().recv();
             match message {
                 Ok(job) => {
-                    println!("Worker {} got a job; executing.", id);
+                    // println!("Worker {} got a job; executing.", id);
                     job();
                 }
                 Err(err) => {
--- http/src/main.rs
+++ http/src/main.rs
@@ -6,14 +6,9 @@ fn main() {
     let pool = ThreadPool::new(4);
     let addr = "127.0.0.1:8080";
     let listener = TcpListener::bind(addr).unwrap();
-    loop {
-        let stream = match listener.accept() {
-            Ok((stream, _)) => stream,
-            Err(e) => {
-                println!("Error: {}", e);
-                continue;
-            }
-        };
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
         pool.execute(move || {
             handle_connection(stream);
         });
